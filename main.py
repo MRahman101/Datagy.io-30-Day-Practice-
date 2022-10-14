473774519acda829853f25bd7df1ee1dc113113f
Md Rahman <83081279+MRahman101@users.noreply.github.com>
2022-10-14 20:31:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+print('Welcome to Online IDE!! Happy Coding :)')
+Replit_john56 = "sup"
+print(Replit_john56)
+print(20 % 3)
+
+
+def john():
+  print("John Cena")
+
+
+john()
+
+
+def color(x):
+  print("my favorite color is   " + x)
+
+
+color("sunshine")
+
+
+#This does calculations
+def add_nums(x, y):
+
+  mod = x % y
+  print(x + y)
+  print(mod)
+
+
+add_nums(35, 20)
+
+# help(add_nums) --> This helps describe what python function does.
+
+#Building A Python Top Calculator 
+def tip_calc(princip, tipcent):
+   tip = princip*tipcent
+  
+   print(tip)
+  
+   
+tip_calc(10.99,.15)
